6af5f442e99b4f6d58953f35e66b41215049dd15
ElizabethSilva <elizabeth.silva.cristina@gmail.com>
2021-08-24 19:44:53 +0000

    arquivo configurado

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,7 +1,11 @@
 package application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class application {
     public static void main(String args[]){
+
+        SpringApplication.run(Application.class, args);
 
     }
 }
