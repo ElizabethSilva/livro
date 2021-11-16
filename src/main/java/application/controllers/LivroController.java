4a39840c590becee8bd2ef70feb98b2f48340772
ElizabethSilva <elizabeth.silva.cristina@gmail.com>
2021-11-16 18:06:38 +0000

    Delete

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,6 +1,7 @@
-package application.controllers;
+package application.controllers;//pacote responsável por fazer a intermediação
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,42 +13,54 @@ import application.models.Livro;
 import application.repositories.LivroRepository;
 import org.springframework.ui.Model;
 
-
 @Controller
 @RequestMapping("/livro")
 public class LivroController {
-    @Autowired
-    private LivroRepository livrosRepo;
-    @RequestMapping("/list")
-    public String list(Model model) {
-        model.addAttribute("livros", livrosRepo.findAll());
-        return "list.jsp";
-    }
-  public String listar(){
+  @Autowired
+  private LivroRepository livrosRepo;
+
+  @RequestMapping("/list")
+  public String list(Model model) {
+    model.addAttribute("livros", livrosRepo.findAll());
+    return "list.jsp";
+
+  }
+
+  public String listar() {
     return "livro/list.jsp";
   }
+
   @RequestMapping("/insert")
-  public String formInsert(){
+  public String formInsert() {
     return "insert.jsp";
 
   }
-  @RequestMapping(value="/insert",method=RequestMethod.POST)
-  public String saveInsert(@RequestParam("titulo")String titulo){
-    
+
+  @RequestMapping(value = "/insert", method = RequestMethod.POST)
+  public String saveInsert(@RequestParam("titulo") String titulo) {
+
     Livro livro = new Livro();
     livro.setTitulo(titulo);
     livrosRepo.save(livro);
     return "redirect:/livro/list";
 
   }
+
   @RequestMapping("/delete/{id}")
-  public String formDelete(Model model, @PathVariable int id){
-    Optional<Livro>livro=livrosRepo.findById(id);
+  public String formDelete(Model model, @PathVariable int id) {
+    Optional<Livro> livro = livrosRepo.findById(id);
     if (!livro.isPresent())
       return "redirect:/livro/list";
-      model.addAttribute("livro",livro.get());
+    model.addAttribute("livro", livro.get());
 
     return "/livro/delete.jsp";
+
+  }
+
+  @RequestMapping(value = "/delete", method = RequestMethod.POST)
+  public String confirmDelete(@RequestParam("id") int id) {
+    livrosRepo.deleteById(id);
+    return "redirect:/livro/list";
 
   }
 }
