d4790d9a7ecd12e2dd2f7563f6e1d560369364f7
ElizabethSilva <elizabeth.silva.cristina@gmail.com>
2021-11-23 19:09:09 +0000

    Comentários

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,89 +1,86 @@
-package application.controllers; //pacote responsável por fazer a intermediação
-import java.util.Optional; //import do pacote java 
+package application.controllers;//pacote responsável por fazer a intermediação
+import java.util.Optional; //import do Optional do Java
 
-//Da linha 5 a 11 estao os imports de bibliotecas e fremeworks
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired; //import do Autowired do springframerwork
+import org.springframework.stereotype.Controller; //import do controlador de tipo de anotação, normalmente usado em combinação com métodos de manipulador anotados com base na RequestMapping
+import org.springframework.web.bind.annotation.PathVariable; //import do PathVariable do springframerwork
+import org.springframework.web.bind.annotation.RequestMapping; //import do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações
+import org.springframework.web.bind.annotation.RequestMethod; //import do RequestMethod
+import org.springframework.web.bind.annotation.RequestParam; //import do RequestParam
 
-//Na linha 14 e 15 é feito o importe de aplicacoes e direcionamento 
-import application.models.Livro;
-import application.repositories.LivroRepository;
+import application.models.Livro; //import do model livro da aplicação
+import application.repositories.LivroRepository; //import do LivroRepository da aplicação
+import org.springframework.ui.Model; //import do model do springframework
 
 
-@Controller //inicializando o Controller.
-@RequestMapping("/livro")
-public class LivroController {
-    @Autowired 
-    private LivroRepository livrosRepo;
-    @RequestMapping("/list")
-    public String list(Model model){ // Este metodo é responsavel por pegar na base de dados o que tem la 
-        model.addAttribute("livros", livrosRepo.findAll()); //aqui pega a tabela do banco de dados livros por meio do objeto da linha 12 e vai encontrar todos os dados
-        return "list.jsp"; // retornar no list
+@Controller //anotação Controller
+@RequestMapping("/livro")//realiza o mapeamento, o controlador receberá requisições no endereço indicado ("/livro")
+public class LivroController { //declaração da classe LivroController
+    @Autowired //anotação para injeção de dependências automáticas
+    private LivroRepository livrosRepo; //criando o objeto privado livrosRepo do tipo LivroRepository
+    @RequestMapping("/list") //realiza o mapeamento, o controlador receberá requisições no endereço indicado ("/list").
+    public String list(Model model){ // Este metodo é responsavel por criando uma função pública list, com parâmetro model, que tem como função a listagem de todos os livro
+        model.addAttribute("livros", livrosRepo.findAll()); ///adicionando o atributo livros no modelo e preenchendo com todos livros do livrosRepo
+        return "list.jsp"; // retorna o arquivo list.jsp
         
     }
-    public String listar(){ //criacao de um novo metodo como o nome listar
-        return "livro/list.jsp";    // retornar no livro/list.jsp
+    public String listar(){ //criacao de um novo metodo responsavel por criando uma função pública listar
+        return "livro/list.jsp"; //retorna o arquivo list.jsp que se encontra na pasta livro.
     }
-
-@RequestMapping("/insert")
-public String formInsert(){ 
-    return "insert.jsp";    // retornar no insert
-
-}
-@RequestMapping(value="/insert",method=RequestMethod.POST)
-public String saveInsert(@RequestParam("titulo")String titulo){
-
-    Livro livro=new Livro();
-    livro.setTitulo(titulo);
-    livrosRepo.save(livro);
-    return "redirect:/livro/list"; // retornar no direcionamento livro/list
-
-}
-@RequestMapping("/delete/{id}")
-public String formDelete(Model model,@PathVariable int id){
-    Optional<Livro>livro=livrosRepo.findById(id);
-    if (!livro.isPresent()) 
-        return "redirect:/livro/list";  // retornar no direcionamento livro/list
-     model.addAttribute("livro",livro.get());
- 
-    return "/livro/delete.jsp"; // retornar livro/delete
+    @RequestMapping("/insert") //realiza o mapeamento, o controlador receberá requisições no endereço indicado ("/insert").
+    public String formInsert(){ //criando função pública sem parâmetro para mostrar o caminho do insert.jsp.
+        return "insert.jsp"; //retorna o arquivo insert.jsp.
     
-
-}
-@RequestMapping(value = "/delete",method=RequestMethod.POST)
-public String confirmDelete(@RequestParam("id")int id){
-   livrosRepo.deleteById(id);
-        return "redirect:/livro/list";  // retornar no direcionamento livro/list
+    }
+    @RequestMapping(value="/insert",method=RequestMethod.POST) //realiza o mapeamento, o controlador receberá requisições do tipo post no endereço indicado ("/insert").
+    public String saveInsert(@RequestParam("titulo")String titulo){ //criando função pública saveInsert, que tem como finalidade inserir um livro no livrosRepo.
+    
+        Livro livro=new Livro(); //criando método livro do tipo Livro.
+        livro.setTitulo(titulo); //configurando o título para livro.
+        livrosRepo.save(livro); //salvando o livro em livrosRepo.
+        return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+    
+    }
+    @RequestMapping("/delete/{id}") //realiza o mapeamento, o controlador receberá requisições no endereço indicado ("/delete/{id}").
+    public String formDelete(Model model,@PathVariable int id){ //criando função pública formDelete, que recebe o id do livro como parâmetro, tem finalidade de fazer a confirmação das possíveis remoções de títulos do repositório.
+        Optional<Livro>livro=livrosRepo.findById(id); //realizando busca pelo id para verificar a existência do livro.
+        if (!livro.isPresent()) //se o livro não estiver presente.
+            return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+         model.addAttribute("livro",livro.get()); //adiciona o atributo livro ao modelo e pega o livro.
      
-
-}
-@RequestMapping("/update/{id}")
-public String formUpdate(Model model,@PathVariable int id){
-    Optional<Livro>livro=livrosRepo.findById(id);
-    if (!livro.isPresent()) 
-        return "redirect:/livro/list";  // retornar no direcionamento livro/list
-     model.addAttribute("livro",livro.get());
- 
-    return "/livro/update.jsp"; // retornar no  livro/update
+        return "/livro/delete.jsp"; //redirecionando para o arquivo delete.jsp que se encontra na pasta livro.
+        
     
-
-}
-@RequestMapping(value = "/update",method=RequestMethod.POST)
-public String saveUpdate(@RequestParam("titulo")String titulo,@RequestParam("id")int id){
-    Optional<Livro>livro=livrosRepo.findById(id);
-    if (!livro.isPresent()) 
-        return "redirect:/livro/list";  // retornar no direcionamento livro/list
-     livro.get().setTitulo(titulo);
-     livrosRepo.save(livro.get());
- 
-     return "redirect:/livro/list"; // retornar no direcionamento livro/list
+    }
+    @RequestMapping(value = "/delete",method=RequestMethod.POST) //realiza o mapeamento, o controlador receberá requisições do tipo post no endereço indicado ("/delete").
+    public String confirmDelete(@RequestParam("id")int id){ //criando  função pública  confirmDelete() com o parâmetro id, que serve para deletar o livro do repositório.
+       livrosRepo.deleteById(id); //deleta o livro do repositório pelo id.
+            return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+         
     
+    }
+    @RequestMapping("/update/{id}") //realiza o mapeamento, o controlador receberá requisições no endereço indicado ("/update/{id}").
+    public String formUpdate(Model model,@PathVariable int id){ //criando função pública formUpdate, que recebe o id do livro como parâmetro, tem finalidade de fazer a confirmação das possíveis atualizações de títulos do repositório.
+        Optional<Livro>livro=livrosRepo.findById(id); //realizando busca pelo id para verificar a existência do livro.
+        if (!livro.isPresent()) //se o livro não estiver presente.
+            return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+         model.addAttribute("livro",livro.get()); //adiciona o atributo livro ao modelo e pega o livro.
      
-
-}
-}
+        return "/livro/update.jsp"; //redirecionando para o arquivo update.jsp que se encontra na pasta livro.
+        
+    
+    }
+    @RequestMapping(value = "/update",method=RequestMethod.POST) //realiza o mapeamento, o controlador receberá requisições do tipo post no endereço indicado ("/update").
+    public String saveUpdate(@RequestParam("titulo")String titulo,@RequestParam("id")int id){ //criando função pública saveUpdate() com os parâmetros titulo e id, que tem como finalidade atualizar os livros do repositório.
+        Optional<Livro>livro=livrosRepo.findById(id); //realizando busca pelo id para verificar a existência do livro.
+        if (!livro.isPresent()) //se o livro não estiver presente.
+            return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+         livro.get().setTitulo(titulo); //se o livro existir, é realizada a alteração de dados.
+         livrosRepo.save(livro.get()); //salvando o livro em livrosRepo.
+     
+         return "redirect:/livro/list"; //redirecionando para o arquivo list.jsp que se encontra na pasta livro.
+        
+         
+    
+    }
+    }
--- src/main/java/application/models/Livro.java
+++ src/main/java/application/models/Livro.java
@@ -1,32 +1,32 @@
-package application.models;
+package application.models; //pacote para manipulação de informações.
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Entity; //import do Entity do Java.
+import javax.persistence.GeneratedValue; //import do GeneratedValue do Java.
+import javax.persistence.GenerationType; //import do GenerationType do Java.
+import javax.persistence.Id; //import do Id do Java.
+import javax.persistence.Table; //import do Table do Java.
 
-@Entity
-@Table(name="livros")
-public class Livro {
+@Entity //anotação para indicar que a classe livro é uma entidade.
+@Table(name="livros") //anotação que especifíca a tabela para o Entity.
+public class Livro { //criando classe Livro.
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id; //Variavel id tipo inteiro 
-    private String titulo; //Variavel titulo tipo string 
+    @Id //anotação que especifíca a chave primário da Entity.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //informando a geração do valor do identificador único.
+    private int id; //criando variável id.
+    private String titulo; //criando variável titulo.
 
-    public int getId() { //O GET recebe as informacoes 
-        return id; //retorna o id
+    public int getId() { //getId tem como funcionalidade pegar o valor de id.
+        return id; //retorna o id.
     }
-    public void setId(int id) { 
-        this.id = id;
+    public void setId(int id) { //setId tem como funcionalidade definir o valor de id.
+        this.id = id; //configura o valor de id, conforme o passado no parâmetro.
     }
-    public String getTitulo() {
-        return titulo; //retorna o titulo
+    public String getTitulo() { //getTitulo tem como funcionalidade pegar o valor de titulo.
+        return titulo; ////retorna o titulo.
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setTitulo(String titulo) { //setTitulo tem como funcionalidade definir o valor de titulo.
+        this.titulo = titulo; ////configura o valor de titulo, conforme o passado no parâmetro.
     }
 
 
